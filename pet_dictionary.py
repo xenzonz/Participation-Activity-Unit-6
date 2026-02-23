5d4c478997da8962dbc27173144ba9819e40396c
sam cocquyt <samcocquyt04@gmail.com>
2026-02-22 22:19:52 -0500

    capitalized

--- pet_dictionary.py
+++ pet_dictionary.py
@@ -1,14 +1,20 @@
 print("Hello world")
 
 #pet dictionary
-pet_1 = {"animal": "dog",
-         "owner": "sam"}
+pet_1 = {"Animal": "Dog",
+         "Owner": "Samuel"}
 
-pet_2 = {"animal": "cat",
-         "owner": "samuel"}
+pet_2 = {"Animal": "Cat",
+         "Owner": "Michael"}
+
+pet_3 = {"Animal": "Parrot",
+         "Owner": "Benjamin"}
+
+pet_4 = {"Animal": "Hamster",
+         "Owner": "John"}
 
 #all pets in a list
-pets = [pet_1, pet_2]
+pets = [pet_1, pet_2, pet_3, pet_4]
 
 #print
 for pet in pets:
