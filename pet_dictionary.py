166d90322f751b1ea64d4cf55786cc6c9fff074f
sam cocquyt <samcocquyt04@gmail.com>
2026-02-22 22:15:04 -0500

    for loop added

--- pet_dictionary.py
+++ pet_dictionary.py
@@ -3,10 +3,14 @@ print("Hello world")
 #pet dictionary
 pet_1 = {"animal": "dog",
          "owner": "sam"}
+
 pet_2 = {"animal": "cat",
          "owner": "samuel"}
 
 #all pets in a list
 pets = [pet_1, pet_2]
 
-print(pets)
+#print
+for pet in pets:
+    for key, value in pet.items():
+        print(f"{key}: {value}")
