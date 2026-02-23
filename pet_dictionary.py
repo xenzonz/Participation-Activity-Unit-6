4e7c9a5fdebb74b779e268b8d869df8df3fc79fe
sam cocquyt <samcocquyt04@gmail.com>
2026-02-22 22:11:51 -0500

    pet list added, pet printer

--- pet_dictionary.py
+++ pet_dictionary.py
@@ -1,7 +1,12 @@
 print("Hello world")
 
-
+#pet dictionary
 pet_1 = {"animal": "dog",
          "owner": "sam"}
 pet_2 = {"animal": "cat",
          "owner": "samuel"}
+
+#all pets in a list
+pets = [pet_1, pet_2]
+
+print(pets)
